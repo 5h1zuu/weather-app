eaffa426602ff030f91037d9ce2dc30499003862
Shizuu <coolcoledude111@gmail.com>
2023-01-30 13:47:49 +0000

    all

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,17 +1,28 @@
 async function getTemp(){
     const searchValue = document.getElementById("searchValue").value
-    const weatherDiv = document.querySelector(".weatherDiv")
-    const weatherType = document.getElementById("#weatherType")
+    const weatherType = document.getElementById("weatherType")
     const cityName = document.getElementById("cityName")
     const temperature = document.getElementById("temperature")
     const feel = document.getElementById("feel")
-    const high = document.getElementById("high")
+    const wind = document.getElementById("wind")
     const humidity = document.getElementById("humidity")
+    const mainContent = document.getElementById("mainContent")
+    let conversion = 273.15
 
 	try{
 	const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchValue}&appid=c1b647a23c9bdda2769d0c7cde5b0042`)
 	const weatherTemp = await response.json()
-    
+
+    mainContent.style.display = "flex"
+    weatherType.innerHTML = weatherTemp.weather[0].description 
+    cityName.innerText = weatherTemp.name
+    temperature.innerText = `${(weatherTemp.main.temp - conversion).toFixed(2)} °C`
+    feel.innerText = `Feels like: ${(weatherTemp.main.feels_like - conversion).toFixed(2)} °C`
+    wind.innerText = `Wind: ${weatherTemp.wind.speed}km/h`
+    humidity.innerText = `Humidity: ${weatherTemp.main.humidity}%`
+    console.log(weatherTemp)
+
+
     
 	}
 
@@ -23,3 +34,5 @@ async function getTemp(){
 }
 
 export { getTemp }
+
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import { getTemp } from "./getWeather"
 
-const weatherDiv = document.querySelector(".weatherDiv")
-const weatherType = document.getElementById("#weatherType")
-const cityName = document.getElementById("cityName")
-const temperature = document.getElementById("temperature")
-const feel = document.getElementById("feel")
-const high = document.getElementById("high")
-const humidity = document.getElementById("humidity")
 const searchBtn = document.getElementById("searchBtn")
-
 
 searchBtn.addEventListener("click",getTemp)
